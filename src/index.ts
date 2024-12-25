2de059f1db24fe12c94b2022448b28b73820144b
Thunnn <157337820+poppyer00@users.noreply.github.com>
2024-12-25 11:28:11 +0700

    Setup get request from body

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,9 @@ const port = 3000;
 app.get('/', (req: Request, res: Response) => {
     res.json('Hello, world!');
 });
-
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
 routes(app);
 
 app.listen(port, () => {
