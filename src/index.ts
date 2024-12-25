6cfd707c91c95fbe96df3b0102e7dda1bd348cd3
Thunnn <157337820+poppyer00@users.noreply.github.com>
2024-12-25 10:04:23 +0700

    Set index routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import routes from './routes';
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get('/', (req: Request, res: Response) => {
     res.json('Hello, world!');
 });
+
+routes(app);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+
+export default (app: { use: (arg0: string, arg1: Router) => void }) => {
+}
